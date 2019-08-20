3c9e12a90ff8e10cf9e5588ee5606a7dbb0c09ad
Tiemo Kieft <github@isogram.nl>
2019-08-20 08:03:19 +0200

    Add bumpversion config

--- django_injector/__init__.py
+++ django_injector/__init__.py
@@ -1,4 +1,8 @@
 from .scope import RequestScope, request_scope
 
+
+__version__ = '0.0.1'
+
+
 __all__ = ['RequestScope', 'request_scope']
 default_app_config = 'django_injector.apps.DjangoInjectorConfig'

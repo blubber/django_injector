1bc1582569707a6b58aa00a5691428d21e5cd735
Tiemo Kieft <github@isogram.nl>
2022-06-01 15:30:33 +0200

    Version 0.2.5

--- django_injector/__init__.py
+++ django_injector/__init__.py
@@ -32,7 +32,7 @@ from injector import (
 )
 
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __all__ = ["RequestScope", "request"]
 default_app_config = "django_injector.DjangoInjectorConfig"
 logger = logging.getLogger(__name__)
